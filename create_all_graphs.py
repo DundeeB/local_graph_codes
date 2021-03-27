84a9b58a7c66823e1b27af63c3d5f11a51611211
DundeeB <ligolas@gmail.com>
2021-03-27 10:28:09 +0300

    More organized, saves graphs, add psi convergence

--- create_all_graphs.py
+++ create_all_graphs.py
@@ -13,6 +13,8 @@ size = 30
 params = {'legend.fontsize': size * 0.75, 'figure.figsize': (10, 10), 'axes.labelsize': size, 'axes.titlesize': size,
           'xtick.labelsize': size * 0.75, 'ytick.labelsize': size * 0.75}
 plt.rcParams.update(params)
+colors_rho = {0.85: 'C0', 0.83: 'C1', 0.8: 'C2', 0.78: 'C3', 0.77: 'C4', 0.785: 'C5', 0.775: 'C6',
+              0.75: 'C7'}
 
 
 def join(*args, **kwargs):
@@ -54,7 +56,8 @@ def prepare_lbl(lbl):
     return lbl
 
 
-def plot_corr(rhoH, specific_op, realizations=1, poly_slope=None, single_slope_label=False, *args, **kwargs):
+def plot_corr(rhoH, specific_op, realizations=1, poly_slope=None, single_slope_label=False, *args,
+              **kwargs):
     pol = (poly_slope is not None)
     if pol:
         maxys = []
@@ -81,7 +84,7 @@ def plot_corr(rhoH, specific_op, realizations=1, poly_slope=None, single_slope_l
                 slopes.append(-p[0])
         x = xs[np.argmin([len(x) for x in xs])]
         y = np.mean(ys[:len(x)], 0)
-        plt.loglog(x, y, label=prepare_lbl('rhoH=' + str(rho)), **default_plt_kwargs)
+        plt.loglog(x, y, colors_rho[rho], label=prepare_lbl('rhoH=' + str(rho)), **default_plt_kwargs)
     if pol:
         I = np.argsort(slopes)
         maxys = np.array(maxys)[I]
@@ -106,26 +109,29 @@ def plot_corr(rhoH, specific_op, realizations=1, poly_slope=None, single_slope_l
     return
 
 
-def plot_pos_and_orientation(rhos):
+def plot_pos_and_orientation(rhos_pos, rhos_psi):
     plt.rcParams.update({'figure.figsize': (10, 10)})
     plt.figure()
     corr_ylim = [1e-2, 1]
     corr_xlim = [0.8, 1e2]
 
     plt.subplot(211)
-    plot_corr(rhos, 'psi_14', poly_slope=0.25, N=4e4)
+    plot_corr(rhos_pos, 'Bragg_S', poly_slope=1.0 / 3)
+    plt.ylim(corr_ylim)
+    plt.xlim(corr_xlim)
+    plt.ylabel(prepare_lbl('Bragg_S'))
+    plt.legend([prepare_lbl('rhoH=' + str(r)) for r in rhos_pos] + ['$x^{1/3}$'], loc=3)
+    # ax = plt.gca()
+    # ax.legend_ = None
+
+    plt.subplot(212)
+    plot_corr(rhos_psi, 'psi_14', poly_slope=0.25, N=4e4)
     plt.ylim(corr_ylim)
     plt.xlim(corr_xlim)
     plt.ylabel(prepare_lbl('psi_14'))
-
-    plt.subplot(212)
-    plot_corr(rhos[:-1], 'Bragg_S', poly_slope=1.0 / 3)
-    plt.ylim(corr_ylim)
-    plt.xlim(corr_xlim)
-    ax = plt.gca()
-    ax.legend_ = None
     plt.xlabel('$\Delta$r/$\sigma$')
-    plt.ylabel(prepare_lbl('Bragg_S'))
+    plt.legend([prepare_lbl('rhoH=' + str(r)) for r in rhos_psi] + ['$x^{1/4}$'], loc=3)
+
     plt.savefig('graphs/orientation_and_position_corr')
 
 
@@ -182,32 +188,44 @@ def quiver_burger(rhoH, xlim, ylim, realization=None, bonds=False, *args, **kwar
 
 
 def plt_cv(rhoH, *args, **kwargs):
-    default_plt_kwargs['linewidth'] = 3
+    default_plt_kwargs['linewidth'] = 5
     op_dir = op_path(rhoH, specif_op='Ising_k=4_undirected', *args, **kwargs)
     sim = join(sims_dir, sim_name(rhoH, *args, **kwargs))
     cvfiles, reals = sort_prefix(op_dir, 'Cv_vs_J')
     Cvs = []
     for file, realization in zip(cvfiles, reals):
         cv_mat = np.loadtxt(join(op_dir, file))
-        ising_op = Ising(sim_path=sim, k_nearest_neighbors=4, directed=False,
-                         centers=np.loadtxt(join(sim, str(realization))), spheres_ind=realization)
-        z = ising_op.bonds_num / ising_op.N
+        # ising_op = Ising(sim_path=sim, k_nearest_neighbors=4, directed=False,
+        #                  centers=np.loadtxt(join(sim, str(realization))), spheres_ind=realization)
+        # z = ising_op.bonds_num / ising_op.N
+        # TODO: rerun with correct z when everything will be ready (its a very long calculation...)
+        z = 3.5
         J = cv_mat[:-1, 0] + np.diff(cv_mat[:, 0]) / 2
         dfdJ = np.diff(cv_mat[:, 2]) / np.diff(cv_mat[:, 0])
         Cvs.append(2 * J ** 2 * z * dfdJ)
 
     min_length = min([len(Cv) for Cv in Cvs])
     Cv = np.mean([Cv[:min_length] for Cv in Cvs], 0)
-    plt.plot(J, Cv, label=prepare_lbl('rhoH=' + str(rhoH)), **default_plt_kwargs)
+    J = J[:min_length]
+    plt.plot(J, Cv, colors_rho[rhoH], label=prepare_lbl('rhoH=' + str(rhoH)), **default_plt_kwargs)
 
 
-def frustration(rhoH, *args, **kwargs):
+def frustration(rhoH, frustration_realizations=5, *args, **kwargs):
     op_dir = op_path(rhoH, 'Graph', *args, **kwargs)
-    spheres_frustration = np.loadtxt(join(op_dir, sort_prefix(op_dir, 'frustration_k=4_undirected_')[0][0]))
+    frustrations = []
+    for i, sp in enumerate(sort_prefix(op_dir, 'frustration_k=4_undirected_')[0]):
+        if i >= frustration_realizations:
+            break
+        frustrations.append(np.loadtxt(join(op_dir, sp)))
+    spheres_frustration = np.mean(frustrations)
 
     op_dir = op_path(rhoH, 'Ising_k=4_undirected', *args, **kwargs)
-    cv_file = sort_prefix(op_dir, 'Cv_vs_J_')[0][0]
-    J, Cv, frustration = np.loadtxt(join(op_dir, cv_file), unpack=True, usecols=(0, 1, 2))
+    cv_files, _ = sort_prefix(op_dir, 'Cv_vs_J_')
+    frusts = []
+    for cv_file in cv_files:
+        _, Cv, f = np.loadtxt(join(op_dir, cv_file), unpack=True, usecols=(0, 1, 2))
+        frusts.append(f[np.argmax(Cv)])
+    f_argmaxcv = np.mean(frusts)
 
     anneal_file = sort_prefix(op_dir, 'anneal_')[0][0]
     A = np.loadtxt(os.path.join(op_dir, anneal_file))
@@ -217,12 +235,12 @@ def frustration(rhoH, *args, **kwargs):
         m = min(A[:, i])
         if m < min_f:
             min_f = m
-    return min_f, spheres_frustration, frustration[np.argmax(Cv)],
+    return min_f, spheres_frustration, f_argmaxcv
 
 
 def plot_ising(rhos, *args, **kwargs):
-    plt.rcParams.update({'figure.figsize': (10, 10), 'axes.labelsize': 0.5 * size, 'xtick.labelsize': size * 0.5,
-                         'ytick.labelsize': size * 0.5, 'legend.fontsize': size * 0.5})
+    plt.rcParams.update({'figure.figsize': (10, 10), 'axes.labelsize': 0.7 * size, 'xtick.labelsize': size * 0.5,
+                         'ytick.labelsize': size * 0.5, 'legend.fontsize': size * 0.6})
     plt.figure()
 
     plt.subplot(211)
@@ -234,29 +252,46 @@ def plot_ising(rhos, *args, **kwargs):
     plt.legend()
 
     plt.subplot(212)
-    default_plt_kwargs['linewidth'] = 3
-    N9e4dirs = [s for s in os.listdir(sims_dir) if s.startswith('N=9')]
+    default_plt_kwargs['linewidth'] = 5
+    N9e4dirs = [s for s in os.listdir(sims_dir) if s.startswith('N=9') and s.find('square') > 0]
     rhos = [float(re.split('(=|_)', dirname)[10]) for dirname in N9e4dirs]
     ground_state, spheres_frustration, max_cv_frustration = [], [], []
     for rho in rhos:
-        a, b, c = frustration(rho)
+        try:
+            a, b, c = frustration(rho)
+        except Exception as err:
+            print(err)
+            a, b, c = np.nan, np.nan, np.nan
         ground_state.append(a)
         spheres_frustration.append(b)
         max_cv_frustration.append(c)
     rhos = np.array(rhos)
-    I = np.argsort(rhos)
-    rhos = rhos[I]
-    ground_state = np.array(ground_state)[I]
-    spheres_frustration = np.array(spheres_frustration)[I]
-    max_cv_frustration = np.array(max_cv_frustration)[I]
-    plt.plot(rhos, ground_state, label='ground state', **default_plt_kwargs)
-    plt.plot(rhos, spheres_frustration, label='spheres heights frustration', **default_plt_kwargs)
-    plt.plot(rhos, max_cv_frustration, '.k', **default_plt_kwargs)
-    p = np.polyfit(rhos, max_cv_frustration, 3)
-    rhos_p = np.linspace(min(rhos), max(rhos))
-    plt.plot(rhos_p, np.polyval(p, rhos_p), 'k', label='frustration at maximal $C_V$', **default_plt_kwargs)
 
-    plt.legend()
+    def filter(I, rhos, ground_state, spheres_frustration, max_cv_frustration):
+        return rhos[I], np.array(ground_state)[I], np.array(spheres_frustration)[I], np.array(max_cv_frustration)[I]
+
+    rhos, ground_state, spheres_frustration, max_cv_frustration = filter(np.argsort(rhos), rhos, ground_state,
+                                                                         spheres_frustration, max_cv_frustration)
+    rhos, ground_state, spheres_frustration, max_cv_frustration = filter(np.logical_not(np.isnan(spheres_frustration)),
+                                                                         rhos, ground_state, spheres_frustration,
+                                                                         max_cv_frustration)
+    plt.plot(rhos, ground_state, label='ground state', **default_plt_kwargs)
+    plt.plot(rhos, spheres_frustration, label='spheres heights', **default_plt_kwargs)
+    # plt.plot(rhos, max_cv_frustration, '.k', **default_plt_kwargs)
+    # rhos_specific = [0.75, 0.8, 0.85]
+    # max_cv_frustration_specific = [f for f, rho in zip(max_cv_frustration, rhos) if rho in rhos_specific]
+    # plt.plot(rhos_specific, max_cv_frustration_specific, '.k', **default_plt_kwargs)
+    rhos, ground_state, spheres_frustration, max_cv_frustration = filter(np.logical_not(np.isnan(max_cv_frustration)),
+                                                                         rhos, ground_state, spheres_frustration,
+                                                                         max_cv_frustration)
+    p = np.polyfit([-r for r in rhos] + [r for r in rhos], 2 * [f for f in max_cv_frustration], 3)
+    # Weired fit with mirror around zero promise monotonic property to fit
+    rhos_p = np.linspace(min(rhos), max(rhos))
+    plt.plot(rhos_p, np.polyval(p, rhos_p), '--k', label='argmax($C_V$) fit', **default_plt_kwargs)
+
+    plt.legend(loc=1)
+    # plt.ylim([0, 0.7])
+    # plt.xlim([0.1, 1])
     plt.grid()
     plt.ylabel('frustration')
     plt.xlabel(prepare_lbl('rhoH'))
@@ -265,7 +300,7 @@ def plot_ising(rhos, *args, **kwargs):
 
 
 def hist_local_psi(rhoH, rad=30, *args, **kwargs):
-    default_plt_kwargs['linewidth'] = 3
+    default_plt_kwargs['linewidth'] = 5
     op_dir = op_path(rhoH, 'Local_psi_14', *args, **kwargs)
     files, reals = sort_prefix(op_dir, 'hist_rad=' + str(rad))
     rhos, psis = [], []
@@ -278,29 +313,33 @@ def hist_local_psi(rhoH, rad=30, *args, **kwargs):
     for rho, psi in zip(rhos, psis):
         psi_avg += np.interp(rho_avg, rho, psi)
     psi_avg /= len(psis)
-    plt.plot(rho_avg, psi_avg, label=prepare_lbl('rhoH=' + str(rhoH)), **default_plt_kwargs)
+    rho_avg = [0] + [r for r in rho_avg]
+    psi_avg = [0] + [p for p in psi_avg]
+    plt.plot(rho_avg, psi_avg, colors_rho[rhoH], label=prepare_lbl('rhoH=' + str(rhoH)), **default_plt_kwargs)
 
 
 def plot_local_psi_hist(rhos, rad=30, *args, **kwargs):
     plt.rcParams.update(params)
     plt.rcParams.update({'figure.figsize': (10, 10)})
     plt.figure()
-    for rho in rhos:
+    for i, rho in enumerate(rhos):
         hist_local_psi(rho, rad, *args, **kwargs)
     plt.legend()
     plt.grid()
-    plt.xlabel('|$\\psi_{14}^{r=' + str(rad) + '}$|')
+    plt.xlabel('|$\\psi_{14}^{r=' + str(int(rad / 2)) + '\sigma}$|')
     plt.ylabel('pdf')
     plt.savefig('graphs/local_psi_histogram')
 
 
 def plot_magnetic_corr(rhos):
+    default_plt_kwargs['linewidth'] = 5
     plt.rcParams.update({'figure.figsize': (12, 10)})
     plt.figure()
     corr_ylim = [1e-2, 1]
     corr_xlim = [0.8, 1e2]
 
     plot_corr(rhos, 'Bragg_Sm', poly_slope=1.0 / 3, single_slope_label=True)
+    plt.legend([prepare_lbl('rhoH=' + str(r)) for r in rhos] + ['$x^{1/3}$'])
     plt.ylim(corr_ylim)
     plt.xlim(corr_xlim)
     plt.xlabel('$\Delta$r/$\sigma$')
@@ -309,8 +348,8 @@ def plot_magnetic_corr(rhos):
 
 
 def plot_bragg_peak(rhoH, *args, **kwargs):
-    plt.rcParams.update({'figure.figsize': (10, 10), 'axes.labelsize': 0.5 * size, 'xtick.labelsize': size * 0.5,
-                         'ytick.labelsize': size * 0.5, 'legend.fontsize': size * 0.5})
+    plt.rcParams.update({'figure.figsize': (10, 10), 'axes.labelsize': 0.45 * size, 'xtick.labelsize': size * 0.38,
+                         'ytick.labelsize': size * 0.4, 'legend.fontsize': size * 0.4, 'axes.titlesize': size * 0.6})
     fig = plt.figure()
     axs = [fig.add_subplot(2, 1, 1, projection='3d'), fig.add_subplot(2, 1, 2, projection='3d')]
     for bragg_type, sub in zip(['Bragg_S', 'Bragg_Sm'], [0, 1]):
@@ -322,7 +361,7 @@ def plot_bragg_peak(rhoH, *args, **kwargs):
             ax.scatter(kx, ky, S_values, '.')
             if sub == 0:
                 ax.set_title(prepare_lbl('rhoH=' + str(rhoH)))
-                ax.set_zlabel('S(k)')
+                ax.set_zlabel('$S(k)$')
             else:
                 ax.set_zlabel('$S^M(k)$')
             ax.set_xlabel('$k_x$')
@@ -333,6 +372,7 @@ def plot_bragg_peak(rhoH, *args, **kwargs):
 
 
 def plot_annealing(rhoH, real=None, *args, **kwargs):
+    plt.figure()
     plt.rcParams.update(params)
     plt.rcParams.update({'figure.figsize': (12, 10)})
     default_plt_kwargs['linewidth'] = 2
@@ -358,15 +398,47 @@ def plot_annealing(rhoH, real=None, *args, **kwargs):
     plt.savefig('graphs/anneal')
 
 
+def converge_psi(rhoH, *args, **kwargs):
+    op_dir = op_path(rhoH, specif_op='psi_14', *args, **kwargs)
+    files, reals = sort_prefix(op_dir, 'vec_', reverse=False)
+    psi_avg = []
+    for file in files:
+        psi_avg.append(np.abs(np.mean(np.loadtxt(join(op_dir, file), dtype=complex))))
+    return psi_avg, reals
+
+
+def plot_psi_convergence(rhos):
+    plt.figure()
+    plt.rcParams.update(params)
+    plt.rcParams.update({'figure.figsize': (12, 10)})
+    default_plt_kwargs['linewidth'] = 5
+
+    def plt_rho(rho, label_surfix, s='-', **keywargs):
+        psi_avg, reals = converge_psi(rho, **keywargs)
+        plt.plot(reals, psi_avg, s + colors_rho[rho], label=prepare_lbl('rhoH=' + str(rho)) + ', ' + label_surfix,
+                 **default_plt_kwargs)
+
+    for rho in rhos:
+        plt_rho(rho, 'N=9e4, square initial conditions')
+        plt_rho(rho, 'N=4e4, honeycomb initial conditions', s='--', initial_conditions='AF_triangle', N=4e4)
+    plt.legend()
+    plt.xlabel('realization')
+    plt.ylabel('$\\overline{\\psi_{14}}$')
+    plt.grid()
+    plt.savefig('graphs/psi_convergence')
+
+
+# TODO: convergence graph sum(psi) from square and honeycomn initial conditions
+
 if __name__ == "__main__":
-    # plot_pos_and_orientation([0.85, 0.83, 0.8, 0.77])
+    # plot_pos_and_orientation([0.85, 0.83, 0.8], [0.8, 0.78, 0.77])
     # quiver_burger(0.8, [85, 106.7], [126, 140.1], realization=64155333, bonds=True)
     # plot_ising([0.85, 0.8, 0.75])
-    # plot_local_psi_hist([0.77, 0.78, 0.8])
 
-    # plot_local_psi_hist([0.77, 0.775, 0.78, 0.785, 0.8])
+    # plot_local_psi_hist([0.8, 0.785, 0.78, 0.775, 0.77])
     # plot_magnetic_corr([0.85, 0.83, 0.8, 0.77])
     # plot_bragg_peak(0.8)
-    plot_annealing(0.8, real=45738368)
+    # plot_annealing(0.8, real=45738368)
+    plot_psi_convergence([0.78, 0.8, 0.83])
 
 plt.show()
